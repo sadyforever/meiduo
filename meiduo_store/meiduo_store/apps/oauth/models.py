458f09bef490987d6291c426032714a3226f98ff
sunsady <405363219@qq.com>
2018-05-20 17:44:52 +0800

    QQ登录通过code获取openid

--- meiduo_store/meiduo_store/apps/oauth/models.py
+++ meiduo_store/meiduo_store/apps/oauth/models.py
@@ -1,10 +1,15 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
 
 from django.db import models
+from itsdangerous import BadData,TimedJSONWebSignatureSerializer as TJWSSerializer
+
 from meiduo_store.utils.models import BaseModel
+from oauth import constants
+
 
 class OAuthQQUser(BaseModel):
     """
@@ -17,3 +22,30 @@ class OAuthQQUser(BaseModel):
         db_table = 'tb_oauth_qq'
         verbose_name = 'QQ登录用户数据'
         verbose_name_plural = verbose_name
+
+    @staticmethod
+    def generate_save_user_token(openid):
+        """
+        生成保存用户数据的token
+        :param openid: 用户的openid
+        :return: token
+        """
+        serializer = TJWSSerializer(settings.SECRET_KEY, expires_in=constants.SAVE_QQ_USER_TOKEN_EXPIRES)
+        data = {'openid': openid}
+        token = serializer.dumps(data)
+        return token.decode()
+
+    @staticmethod
+    def check_save_user_token(token):
+        """
+        检验保存用户数据的token
+        :param token: token
+        :return: openid or None
+        """
+        serializer = TJWSSerializer(settings.SECRET_KEY, expires_in=constants.SAVE_QQ_USER_TOKEN_EXPIRES)
+        try:
+            data = serializer.loads(token)
+        except BadData:
+            return None
+        else:
+            return data.get('openid')
--- meiduo_store/meiduo_store/apps/oauth/utils.py
+++ meiduo_store/meiduo_store/apps/oauth/utils.py
@@ -5,6 +5,7 @@ import logging
 import json
 
 # from .exceptions import QQAPIException
+from oauth.exceptions import QQAPIException
 
 logger = logging.getLogger('django')
 
@@ -20,6 +21,7 @@ class OAuthQQ(object):
         self.redirect_url = redirect_url or settings.QQ_REDIRECT_URL
         self.state = state or settings.QQ_STATE
 
+    # 拼接第一次GET的QQ登录地址
     def generate_qq_login_url(self):
         """
         拼接用户QQ登录的链接地址
@@ -38,3 +40,57 @@ class OAuthQQ(object):
         print(url)
 
         return url
+
+    def get_access_token(self, code):
+        """
+        获取qq的access_token
+        :param code: 调用的凭据
+        :return: access_token
+        """
+        url = 'https://graph.qq.com/oauth2.0/token?'
+        req_data = {
+            'grant_type': 'authorization_code',
+            'client_id': self.app_id,
+            'client_secret': self.app_key,
+            'code': code,
+            'redirect_uri': self.redirect_url
+        }
+        url += urlencode(req_data)
+
+        try:
+            # 发送请求
+            response = urlopen(url)
+            # 读取QQ返回的响应体数据
+            # access_token=FE04************************CCE2&expires_in=7776000&refresh_token=88E4************************BE1
+            response = response.read().decode()
+
+            # 将返回的数据转换为字典
+            resp_dict = parse_qs(response)
+
+            access_token = resp_dict.get("access_token")[0]
+        except Exception as e:
+            logger.error(e)
+            raise QQAPIException('获取access_token异常')
+
+        return access_token
+
+    def get_openid(self, access_token):
+        """
+        获取用户的openid
+        :param access_token: qq提供的access_token
+        :return: open_id
+        """
+        url = 'https://graph.qq.com/oauth2.0/me?access_token=' + access_token
+        try:
+            response = urlopen(url)
+            response_data = response.read().decode()
+            # 返回的数据 callback( {"client_id":"YOUR_APPID","openid":"YOUR_OPENID"} )\n;
+            data = json.loads(response_data[10:-4])
+        except Exception:
+            data = parse_qs(response_data)
+            logger.error('code=%s msg=%s' % (data.get('code'), data.get('msg')))
+            raise QQAPIException('获取openid异常')
+
+        openid = data.get('openid', None)
+        return openid
+
